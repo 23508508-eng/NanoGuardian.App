22e6aa09e2e864b46b83555f7f8a738d20ef0868
Alberto <23508508@uagro.mx>
2026-05-13 19:19:48 -0600

    Actualizaación de  dashboard

--- NanoGuardian.App/Models/Alerta.cs
+++ NanoGuardian.App/Models/Alerta.cs
@@ -5,4 +5,4 @@ public class Alerta
     public string Paciente { get; set; } = string.Empty;
     public int FuerzaImpactoG { get; set; }
     public string Estado { get; set; } = string.Empty;
-}
+}//Agregado al final
